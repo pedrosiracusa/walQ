c5d977979c2d7c23b061c1c579c51f5288adbebe
Pedro de Siracusa <pedrosiracusa@gmail.com>
2018-12-08 01:30:20 +0000

    Improving UI for the example webapp client
    Added bootstrap code to static vendor folder (giving up the CDN);
    Added a header;
    Now the application keeps a reference of whether the source of a message
    is the user or the app;
    Added JQuery and Popper files, using CDN.

--- examples/webservice/client.py
+++ examples/webservice/client.py
@@ -13,23 +13,23 @@ import re
 from flask import Flask, request, session, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.secret_key = os.urandom(24)
 
 api_endpoint = 'http://localhost:5000'
 
 
-def updateMessages(session,context_data):
+def updateMessageHistory(session,context_data):
     usrinput=context_data.get('usrinput')
     if usrinput['data']=='':
         if context_data['message'] is None:
             return session
         else:
-            session['messages_history'].append(context_data['message'])
+            session['messages_history'].append( ('owned_app',context_data['message']) )
     elif usrinput['type']=='choice':
-        session['messages_history'].append(usrinput.get('options_text')[ int(usrinput['data']) ])
+        session['messages_history'].append( ('owned_usr', usrinput.get('options_text')[ int(usrinput['data']) ]) )
     else:
-        session['messages_history'].append(usrinput.get('data'))
+        session['messages_history'].append( ('owned_usr', usrinput.get('data')) )
     return session
 
 @app.route('/', methods=['GET','POST'])
@@ -41,8 +41,8 @@ def initialize():
         session['walq_context_data'] = context_data
         session['messages_history'] = list()
         message = context_data.get('message')
-        updateMessages(session,context_data)
-        return render_template('form.html',send_input=True, messages=session['messages_history']) 
+        updateMessageHistory(session,context_data)
+        return render_template('form.html',send_input=True, messages=session['messages_history'], usrdata=context_data['usrdata']) 
         
 
     elif request.method=='POST':
@@ -51,12 +51,12 @@ def initialize():
         context_data = session['walq_context_data'] 
         usr_input = request.form.get('input')
         context_data['usrinput'].update({'data':usr_input})
-        updateMessages(session,context_data)
+        updateMessageHistory(session,context_data)
 
         # Post the context object (with user input set) and receive new context containing the api response
         context_data = requests.post(api_endpoint, data=json.dumps(context_data)).json()
         answer = context_data.get('message')
-        updateMessages(session,context_data)
+        updateMessageHistory(session,context_data)
 
 
         # If this is the last message (no more questions from the api)
@@ -64,7 +64,8 @@ def initialize():
             return render_template('form.html', 
                     messages=session['messages_history'],
                     send_input=False, 
-                    conv_data=context_data.get('usrdata'))
+                    conv_data=context_data.get('usrdata'),
+                    usrdata=context_data['usrdata'])
 
         # If this is not the last message (there are next questions), ask for the next question
         else:
@@ -72,15 +73,16 @@ def initialize():
 
             # Update session data (context)
             session['walq_context_data']=context_data
-            updateMessages(session,context_data)
+            updateMessageHistory(session,context_data)
 
             if context_data.get('usrinput').get('type')=='choice':
                 return render_template('form.html', send_input=True,
                         messages=session['messages_history'],
+                        usrdata=context_data['usrdata'],
                         input_type=context_data.get('usrinput').get('type'), 
                         input_vals=[ list(i) for i in zip(context_data.get('usrinput').get('options'), context_data.get('usrinput').get('options_text')) ])
             else:
-                return render_template('form.html',send_input=True, answer=answer, messages=session['messages_history'])
+                return render_template('form.html',send_input=True, answer=answer, messages=session['messages_history'], usrdata=context_data['usrdata'])
 
 
 
